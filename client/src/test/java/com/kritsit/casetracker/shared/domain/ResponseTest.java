a3c2a9f57df648a92bc882af20f19db9981e43c3
Kyle Robertze <paddatrapper@gmail.com>
2015-02-05 08:40:51 +0200

    moved shared models

---
+++ client/src/test/java/com/kritsit/casetracker/shared/domain/ResponseTest.java
@@ -0,0 +1,29 @@
+package com.kritsit.casetracker.shared.domain;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class ResponseTest extends TestCase {
+    Response response;
+
+    public ResponseTest(String name) {
+        super(name);
+    }
+
+    public static Test suite() {
+        return new TestSuite(ResponseTest.class);
+    }
+
+    public void setUp() {
+        response = new Response(200, "This is a message");
+    }
+
+    public void testGetMessage() {
+        assertTrue(200 == response.getMessage());
+    }
+
+    public void testGetBody() {
+        assertTrue("This is a message".equals(response.getBody()));
+    }
+}
