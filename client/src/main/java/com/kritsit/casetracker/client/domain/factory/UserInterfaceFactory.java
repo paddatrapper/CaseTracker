3ce5666a37f579c16f0302354f2c75e2107e2b8f
Kyle Robertze <paddatrapper@gmail.com>
2015-05-06 11:04:56 +0200

    added UI factory

---
+++ client/src/main/java/com/kritsit/casetracker/client/domain/factory/UserInterfaceFactory.java
@@ -0,0 +1,15 @@
+package com.kritsit.casetracker.client.domain.factory;
+
+import com.kritsit.casetracker.client.domain.ui.EditorFrame;
+import com.kritsit.casetracker.client.domain.ui.IUserInterface;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javafx.stage.Stage;
+
+public class UserInterfaceFactory {
+    public static IUserInterface getEditorFrame() {
+        return new EditorFrame();
+    }
+}
---
+++ client/src/test/java/com/kritsit/casetracker/client/domain/factory/UserInterfaceFactoryTest.java
@@ -0,0 +1,21 @@
+package com.kritsit.casetracker.client.domain.factory;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class UserInterfaceFactoryTest extends TestCase {
+    public UserInterfaceFactoryTest(String name) {
+        super(name);
+    }
+
+    public static Test suite() {
+        return new TestSuite(UserInterfaceFactoryTest.class);
+    }
+
+    public void setUp(){}
+
+    public void testGetEditorFrame() {
+        assertTrue(UserInterfaceFactory.getEditorFrame() != null);
+    }
+}
