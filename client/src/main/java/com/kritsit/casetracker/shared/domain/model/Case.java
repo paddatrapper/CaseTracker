40ba3d5ec644f583c4f7ac8b99315a583437ecd3
Kyle Robertze <paddatrapper@gmail.com>
2015-05-07 12:44:30 +0200

    moved properties to be wrapped in javafx beans properties

--- client/src/main/java/com/kritsit/casetracker/shared/domain/model/Case.java
+++ client/src/main/java/com/kritsit/casetracker/shared/domain/model/Case.java
@@ -1,5 +1,9 @@
 package com.kritsit.casetracker.shared.domain.model;
 
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import java.io.Serializable;
 import java.text.ParseException;
 import java.util.ArrayList;
@@ -9,159 +13,171 @@ import java.util.Objects;
 
 public class Case implements Serializable {
     private static final long serialVersionUID = 10L;
-    private String caseNumber;
-    private String caseName;
-    private String description;
-    private String animalsInvolved;
-    private Staff investigatingOfficer;
-    private Incident incident;
-    private Defendant defendant;
-    private Person complainant;
-    private Date nextCourtDate;
-    private String outcome;
-    private List<Evidence> evidence;
-    private boolean returnVisit;
-    private Date returnDate;
-    private String caseType;
+    private SimpleStringProperty caseNumberProperty;
+    private SimpleStringProperty caseNameProperty;
+    private SimpleStringProperty descriptionProperty;
+    private SimpleStringProperty animalsInvolvedProperty;
+    private SimpleObjectProperty<Staff> investigatingOfficerProperty;
+    private SimpleObjectProperty<Incident> incidentProperty;
+    private SimpleObjectProperty<Defendant> defendantProperty;
+    private SimpleObjectProperty<Person> complainantProperty;
+    private SimpleObjectProperty<Date> nextCourtDateProperty;
+    private SimpleStringProperty outcomeProperty;
+    private SimpleObjectProperty<List<Evidence>> evidenceProperty;
+    private SimpleBooleanProperty returnVisitProperty;
+    private SimpleObjectProperty<Date> returnDateProperty;
+    private SimpleStringProperty caseTypeProperty;
 
     public Case(String caseNumber, String caseName, String description, String animalsInvolved, Staff investigatingOfficer, Incident incident, Defendant defendant, Person complainant, Date nextCourtDate, List<Evidence> evidence, boolean returnVisit, Date returnDate, String caseType, String outcome) {
-        this.caseNumber = caseNumber;
-        this.caseName = caseName;
-        this.description = description;
-        this.animalsInvolved = animalsInvolved;
-        this.investigatingOfficer = investigatingOfficer;
-        this.incident = incident;
-        this.defendant = defendant;
-        this.complainant = complainant;
-        this.nextCourtDate = nextCourtDate;
-        this.evidence = evidence;
-        this.returnVisit = returnVisit;
-        this.returnDate = returnDate;
-        this.caseType = caseType;
-        this.outcome = outcome;
+        setNumber(caseNumber);
+        setName(caseName);
+        setDescription(description);
+        setAnimalsInvolved(animalsInvolved);
+        setInvestigatingOfficer(investigatingOfficer);
+        setIncident(incident);
+        setDefendant(defendant);
+        setComplainant(complainant);
+        setNextCourtDate(nextCourtDate);
+        setEvidence(evidence);
+        setReturnVisit(returnVisit);
+        setReturnDate(returnDate);
+        setType(caseType);
+        setRuling(outcome);
     }
 
     public void addEvidence(Evidence evidence) {
-        this.evidence.add(evidence);
+        evidenceProperty.getValue().add(evidence);
     }
 
     // Accessor methods:
     public String getAnimalsInvolved() {
-        return animalsInvolved;
+        return animalsInvolvedProperty.getValue();
     }
 
     public String getName() {
-        return caseName;
+        return caseNameProperty.getValue();
     }
 
     public String getNumber() {
-        return caseNumber;
+        return caseNumberProperty.getValue();
     }
 
     public String getType() {
-        return caseType;
+        return caseTypeProperty.getValue();
     }
 
     public Person getComplainant() {
-        return complainant;
+        return complainantProperty.getValue();
     }
 
     public String getDescription() {
-        return description;
+        return descriptionProperty.getValue();
     }
 
     public List<Evidence> getEvidence() {
-        return evidence;
+        return evidenceProperty.getValue();
     }
 
     public Incident getIncident() {
-        return incident;
+        return incidentProperty.getValue();
     }
 
     public Staff getInvestigatingOfficer() {
-        return investigatingOfficer;
+        return investigatingOfficerProperty.getValue();
     }
 
     public Date getNextCourtDate() {
-        return nextCourtDate;
+        return nextCourtDateProperty.getValue();
     }
 
     public Defendant getDefendant() {
-        return defendant;
+        return defendantProperty.getValue();
     }
 
     public Date getReturnDate() {
-        return returnDate;
+        return returnDateProperty.getValue();
     }
 
     public String getRuling() {
-        return outcome;
+        return outcomeProperty.getValue();
     }
 
     public boolean isReturnVisit() {
-        return returnVisit;
+        return returnVisitProperty.getValue();
+    }
+
+    public Property caseNumberProperty() {
+        return caseNumberProperty();
+    }
+
+    public Property caseNameProperty() {
+        return caseNameProperty();
+    }
+
+    public Property caseTypeProperty() {
+        return caseTypeProperty();
     }
 
     // Mutator methods:
     public void setAnimalsInvolved(String animalsInvolved) {
-        this.animalsInvolved = animalsInvolved;
+        animalsInvolvedProperty.setValue(animalsInvolved);
     }
 
     public void setName(String name) {
-        this.caseName = name;
+        caseNameProperty.setValue(name);
     }
 
     public void setNumber(String caseNumber) {
-        this.caseNumber = caseNumber;
+        caseNumberProperty.setValue(caseNumber);
     }
 
     public void setType(String caseType) {
-        this.caseType = caseType;
+        caseTypeProperty.setValue(caseType);
     }
 
     public void setComplainant(Person complainant) {
-        this.complainant = complainant;
+        complainantProperty.setValue(complainant);
     }
 
     public void setDescription(String description) {
-        this.description = description;
+        descriptionProperty.setValue(description);
     }
 
     public void setIncident(Incident incident) {
-        this.incident = incident;
+        incidentProperty.setValue(incident);
     }
 
     public void setInvestigatingOfficer(Staff investigatingOfficer) {
-        this.investigatingOfficer = investigatingOfficer;
+        investigatingOfficerProperty.setValue(investigatingOfficer);
     }
 
     public void setNextCourtDate(Date nextCourtDate) {
-        this.nextCourtDate = nextCourtDate;
+        nextCourtDateProperty.setValue(nextCourtDate);
     }
 
     public void setDefendant(Defendant defendant) {
-        this.defendant = defendant;
+        defendantProperty.setValue(defendant);
     }
 
     public void setReturnDate(Date returnDate) {
-        this.returnDate = returnDate;
+        returnDateProperty.setValue(returnDate);
     }
 
     public void setReturnVisit(boolean returnVisit) {
-        this.returnVisit = returnVisit;
+        returnVisitProperty.setValue(returnVisit);
     }
 
     public void setRuling(String outcome) {
-        this.outcome = outcome;
+        outcomeProperty.setValue(outcome);
     }
 
     public void setEvidence(List<Evidence> evidence) {
-        this.evidence = evidence;
+        evidenceProperty.setValue(evidence);
     }
 
     @Override
     public int hashCode() {
-        return ((caseNumber + caseName + description + animalsInvolved + outcome + caseType).hashCode() + investigatingOfficer.hashCode() + incident.hashCode() + defendant.hashCode() + complainant.hashCode()) / 3;
+        return ((getNumber() + getName() + getDescription() + getAnimalsInvolved() + getRuling() + getType()).hashCode() + getInvestigatingOfficer().hashCode() + getIncident().hashCode() + getDefendant().hashCode() + getComplainant().hashCode()) / 3;
     }
 
     @Override
@@ -178,8 +194,8 @@ public class Case implements Serializable {
     @Override
     public String toString() {
         String result = "Case: ";
-        result += caseNumber + " ";
-        result += "(" + caseName + ")";
+        result += getNumber() + " ";
+        result += "(" + getName() + ")";
         return result;
     }
 }
