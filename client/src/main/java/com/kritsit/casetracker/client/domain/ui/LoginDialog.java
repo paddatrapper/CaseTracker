abdb1521ab09a2582acdd9ee4363dcf13bf32ce7
Kyle Robertze <paddatrapper@gmail.com>
2015-05-08 12:35:06 +0200

    added logging

--- client/src/main/java/com/kritsit/casetracker/client/domain/ui/LoginDialog.java
+++ client/src/main/java/com/kritsit/casetracker/client/domain/ui/LoginDialog.java
@@ -5,6 +5,9 @@ import com.kritsit.casetracker.client.domain.services.ILoginService;
 import com.kritsit.casetracker.client.domain.ui.controller.LoginController;
 import com.kritsit.casetracker.shared.domain.model.Staff;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -12,16 +15,22 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class LoginDialog {
+    private static final Logger logger = LoggerFactory.getLogger(LoginDialog.class);
+
     public Staff run(Stage stage) throws IOException {
+        logger.info("Starting the login interface");
+        logger.debug("Loading FXML");
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/ui/fxml/LoginDialog.fxml"));
         Parent root = loader.load();
+        logger.debug("FXML loaded");
         LoginController controller = (LoginController) loader.getController();
         ILoginService loginService = ServiceFactory.getLoginService();
         controller.setLoginService(loginService);
         controller.setStage(stage);
+        logger.debug("Login interface prepared");
         stage.setTitle("CaseTracker - Login");
         stage.setScene(new Scene(root, 300, 275));
-        stage.show();
+        stage.showAndWait();
         return controller.getUser();
     }
 }
