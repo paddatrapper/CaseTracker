a9f068f28c9213a40eebac410654796005c7ae28
Kyle Robertze <paddatrapper@gmail.com>
2015-06-09 14:09:53 +0200

    added server state manipulation scripts

---
+++ scripts/start_server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# Starts the server in the given directory
+#
+
+cd $1/server
+java -jar target/server-0.1a-SNAPSHOT-jar-with-dependencies.jar&
---
+++ scripts/stop_server.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# Stops the server
+#
+
+pkill -f "java -jar target/server-0.1a-SNAPSHOT-jar-with-dependencies.jar"
