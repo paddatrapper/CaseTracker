e95477fd1488d7178c78e82ac3743258be08dffa
Kyle Robertze <paddatrapper@gmail.com>
2015-07-07 16:14:12 +0200

    added file serializer for evidence uploads

---
+++ client/src/main/java/com/kritsit/casetracker/shared/domain/FileSerializer.java
@@ -0,0 +1,40 @@
+package com.kritsit.casetracker.shared.domain;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileSerializer {
+    private final Logger logger = LoggerFactory.getLogger(FileSerializer.class);
+
+    public FileSerializer() {}
+
+    public byte[] serialize(File f) throws IOException {
+        logger.info("Serializing file {}", f.getAbsolutePath());
+        byte[] outputBytes;
+        try (FileInputStream input = new FileInputStream(f);
+                ByteArrayOutputStream output = new ByteArrayOutputStream()) {
+            byte[] b  = new byte[1024];
+            for (int i; (i = input.read(b)) != -1; i++) {
+                output.write(b, 0, i);
+            }
+            outputBytes = output.toByteArray();
+        }
+        return outputBytes;
+    }
+
+    public void write(File f, byte[] bytes) throws IOException {
+        logger.info("Writing to file {}", f.getAbsolutePath());
+        f.mkdirs();
+        f.delete();
+        try (FileOutputStream output = new FileOutputStream(f)) {
+            output.write(bytes);
+            output.flush();
+        }
+    }
+}
---
+++ server/src/test/java/com/kritsit/casetracker/shared/FileSerializerIT.java
@@ -0,0 +1,47 @@
+package com.kritsit.casetracker.shared.domain;
+
+import static org.mockito.Mockito.*;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileSerializerIT extends TestCase {
+    private FileSerializer serializer;
+
+    public FileSerializerIT(String name) {
+        super(name);
+    }
+
+    public static Test suite() {
+        return new TestSuite(FileSerializerIT.class);
+    }
+
+    public void setUp() {
+        serializer = new FileSerializer();
+    }
+
+    public void testSerialize() throws IOException {
+        File f = new File("src/test/resources/file-test.txt");
+        byte[] result = serializer.serialize(f);
+        assertNotNull(result);
+    }
+
+    public void testWrite() throws IOException {
+        File f = new File("src/test/resources/file-test.txt");
+        byte[] result = serializer.serialize(f);
+        File destinationFile = new File("src/test/resources/test-write.txt");
+        serializer.write(destinationFile, result);
+        assertTrue(destinationFile.isFile());
+    }
+
+    public void tearDown() {
+        File f = new File("src/test/resources/test-write.txt");
+        if (f.exists()) {
+            f.delete();
+        }
+    }
+}
