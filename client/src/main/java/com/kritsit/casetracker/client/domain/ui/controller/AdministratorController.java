14795eb0021f012450adec50d225deb5a025a535
pawc <pswid89@gmail.com>
2015-07-22 23:34:14 +0200

    introduction to controller for administrator view

---
+++ client/src/main/java/com/kritsit/casetracker/client/domain/ui/controller/AdministratorController.java
@@ -0,0 +1,73 @@
+package com.kritsit.casetracker.client.domain.ui.controller;
+
+import com.kritsit.casetracker.shared.domain.model.Permission;
+import com.kritsit.casetracker.shared.domain.model.Staff;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.fxml.FXML;
+
+public class AdministratorController implements IController {
+    
+    @FXML private TextField searchField;
+    @FXML private ComboBox<String> searchCombobox;
+    @FXML private Button resetPasswordButton;
+    @FXML private Button editButton;
+    @FXML private Button deleteButton;
+    @FXML private TableView staffTable;
+    private ObservableList<Staff> staffList;
+    @FXML private TextField firstNameField;
+    @FXML private TextField lastNameField;
+    @FXML private ComboBox<String> departmentCombobox;
+    @FXML private TextField positionField;
+    @FXML private TextField usernameField;
+    @FXML private ComboBox<String> permissionCombobox;
+    @FXML private Button addUserButton;
+
+    private void initStaffTable(){
+        TableColumn firstNameColumn = new TableColumn("First name");
+        TableColumn lastNameColumn = new TableColumn("Last name");
+        TableColumn usernameColumn = new TableColumn("Username");
+        TableColumn departmentColumn = new TableColumn("Department");
+        TableColumn positionColumn = new TableColumn("Position");
+        
+        staffTable.getColumns().addAll(firstNameColumn, lastNameColumn,
+                usernameColumn, departmentColumn, positionColumn);
+        
+        firstNameColumn.setCellValueFactory(new PropertyValueFactory<Staff, String>("First name"));
+        lastNameColumn.setCellValueFactory(new PropertyValueFactory<Staff, String>("Last name"));
+        usernameColumn.setCellValueFactory(new PropertyValueFactory<Staff, String>("Username"));
+        departmentColumn.setCellValueFactory(new PropertyValueFactory<Staff, String>("Department"));
+        positionColumn.setCellValueFactory(new PropertyValueFactory<Staff, String>("Position"));
+        
+        staffTable.setItems(staffList);
+    }
+    
+    private void resetAddUserTab(){
+        firstNameField.setText("");
+        lastNameField.setText("");
+        departmentCombobox.setValue("");
+        positionField.setText("");
+        usernameField.setText("");
+        permissionCombobox.setValue("");
+    }
+    
+    private void resetSearch(){
+        searchField.setText("");
+        searchCombobox.setValue("");
+    }
+    
+    private void initPermissionCombobox(){
+       ObservableList<String> permissions = 
+               FXCollections.observableArrayList(Permission.ADMIN.toString(),
+               Permission.EDITOR.toString(), Permission.VIEWER.toString());
+       permissionCombobox = new ComboBox<String>(permissions);
+    }
+}
