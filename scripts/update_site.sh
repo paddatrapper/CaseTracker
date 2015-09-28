c507b55e23d4a4df7e8a15b5ca6240f74e7d237b
Kyle Robertze <paddatrapper@gmail.com>
2015-09-28 23:57:11 +0200

    changed ssh port

--- scripts/update_site.sh
+++ scripts/update_site.sh
@@ -6,14 +6,14 @@
 mvn clean site
 # Remove old files from server
 echo "Removing old site files"
-ssh kyle@kritsit.ddns.net "rm -rf /var/www/kritsit.ddns.net/public_html/casetracker; mkdir -p /var/www/kritsit.ddns.net/public_html/casetracker/{client,server};"
+ssh kyle@kritsit.ddns.net -p 443 "rm -rf /var/www/kritsit.ddns.net/public_html/casetracker; mkdir -p /var/www/kritsit.ddns.net/public_html/casetracker/{client,server};"
 
 # Copy new files to server
 echo "Copying new project site from developer"
-scp -r ./target/site/* kyle@kritsit.ddns.net:/var/www/kritsit.ddns.net/public_html/casetracker/
+scp -r -P 443 ./target/site/* kyle@kritsit.ddns.net:/var/www/kritsit.ddns.net/public_html/casetracker/
 
 echo "Copying new client site from developer"
-scp -r ./client/target/site/* kyle@kritsit.ddns.net:/var/www/kritsit.ddns.net/public_html/casetracker/client/
+scp -r -P 443 ./client/target/site/* kyle@kritsit.ddns.net:/var/www/kritsit.ddns.net/public_html/casetracker/client/
 
 echo "Copying new server site from developer"
-scp -r ./server/target/site/* kyle@kritsit.ddns.net:/var/www/kritsit.ddns.net/public_html/casetracker/server/
+scp -r -P 443 ./server/target/site/* kyle@kritsit.ddns.net:/var/www/kritsit.ddns.net/public_html/casetracker/server/
