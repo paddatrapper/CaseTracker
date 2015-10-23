2a11008a6581e9cef49ef1eb2bae73e8492a72b1
Kyle Robertze <paddatrapper@gmail.com>
2015-10-24 00:25:05 +0200

    fixed formatting

--- client/src/main/java/com/kritsit/casetracker/client/domain/ui/controller/AdministratorController.java
+++ client/src/main/java/com/kritsit/casetracker/client/domain/ui/controller/AdministratorController.java
@@ -63,7 +63,7 @@ import java.util.List;
 import java.util.Map;
 
 public class AdministratorController implements IController {
-    
+
     private ObservableList<Staff> staffList;
     private FilteredList<Staff> filteredStaff;
     private IAdministratorService administratorService;
@@ -71,11 +71,11 @@ public class AdministratorController implements IController {
     private IExportService exportService;
     private Stage stage;
     private final Logger logger = LoggerFactory.getLogger(AdministratorController.class);
-    
+
     public void setAdministratorService(IAdministratorService administratorService) {
         this.administratorService = administratorService;
     }
-    
+
     public void setMenuService(IMenuService menuService){
         this.menuService = menuService;
     }
@@ -83,7 +83,7 @@ public class AdministratorController implements IController {
     public void setExportService(IExportService exportService){
 	this.exportService = exportService;
     }
-    
+
     public void initFrame(){
         logger.info("Initiating frame");
         if (administratorService.getUser().getPermission() == Permission.ADMIN) {
@@ -92,32 +92,32 @@ public class AdministratorController implements IController {
             initSearchCombobox();
         } else {
             logger.debug("Administrator view disabled");
-        } 
+        }
     }
-    
+
     public void initialize(){
-        
+
         changePasswordItem.setOnAction(event->{
             menuService.changePasswordFrame();
         });
-        
+
         newUserItem.setOnAction(event->{
             SingleSelectionModel<Tab> selection = tabPane.getSelectionModel();
             selection.select(addUserTab);
         });
-        
+
         editUserItem.setOnAction(event->{
             editUser();
         });
-        
+
         deleteUserItem.setOnAction(event->{
             deleteUser();
         });
-        
+
         resetPasswordItem.setOnAction(event->{
             resetPassword();
         });
-        
+
         logoutItem.setOnAction(event->{
             stage.close();
             menuService.restart();
@@ -129,19 +129,19 @@ public class AdministratorController implements IController {
         //TODO
         helpItem.setDisable(true);
         aboutItem.setDisable(true);
-        
+
         btnResetPassword.setOnAction(event->{
             resetPassword();
         });
-        
+
         btnDelete.setOnAction(event->{
           deleteUser();
         });
-        
+
         btnAddUser.setOnAction(event->{
            addUser();
         });
-        
+
         btnEdit.setOnAction(event->{
            editUser();
         });
@@ -189,7 +189,7 @@ public class AdministratorController implements IController {
         inputMap.put("department", cbxAddDepartment.getValue());
         inputMap.put("position", txfAddPosition.getText());
         inputMap.put("permission", cbxAddPermission.getValue());
-        
+
         InputToModelParseResult result = administratorService.addUser(inputMap);
         if(result.isSuccessful()){
             Alert alert = new Alert(AlertType.INFORMATION);
@@ -206,10 +206,10 @@ public class AdministratorController implements IController {
             alert.setHeaderText("Error adding new user");
             alert.setContentText(result.getReason());
             alert.showAndWait();
-        }   
-        
+        }
+
     }
-    
+
     private void editUser(){
         TableViewSelectionModel<Staff> selection =  tblStaff.getSelectionModel();
         if(selection.getSelectedItem()==null){
@@ -217,26 +217,26 @@ public class AdministratorController implements IController {
             alert.setTitle("Editing user");
             alert.setHeaderText("Information");
             alert.setContentText("Select a user to edit");
-            alert.showAndWait();   
+            alert.showAndWait();
             return;
         }
-        
+
         Staff s = selection.getSelectedItem();
         EditUserController c = new EditUserController(s, administratorService, this);
         AnchorPane EditUserPane = null;
         FXMLLoader fxmlLoader = new FXMLLoader(getClass()
                 .getResource("/ui/fxml/EditUser.fxml"));
-        
+
         fxmlLoader.setController(c);
         fxmlLoader.setRoot(EditUserPane);
-        
+
         try{
             EditUserPane= (AnchorPane) fxmlLoader.load();
         } catch(IOException e){
             logger.error("Error loading frame to edit user.", e);
             return;
         }
-        
+
         Scene scene = new Scene(EditUserPane);
         Stage stage = new Stage();
         stage.setTitle("Editing user "+s.getUsername());
@@ -253,7 +253,7 @@ public class AdministratorController implements IController {
             alert.setTitle("Deleting user");
             alert.setHeaderText("Information");
             alert.setContentText("Select a user to delete");
-            alert.showAndWait();   
+            alert.showAndWait();
             return;
         }
         String selectedUsername = selection.getSelectedItem().getUsername();
@@ -287,10 +287,9 @@ public class AdministratorController implements IController {
 
     @SuppressWarnings("unchecked")
     protected void initStaffTable(){
-       
+
         logger.info("Initiating staff list table");
         staffList = FXCollections.observableArrayList(administratorService.getStaff());
-    
         filteredStaff = new FilteredList<>(staffList, p -> true);
         txfFilterUsers.textProperty().addListener((observable, oldValue, newValue) -> {
             filteredStaff.setPredicate(s -> {
@@ -304,7 +303,7 @@ public class AdministratorController implements IController {
                 return false;
             });
         });
-        
+
         cbxFilterPermissions.valueProperty().addListener((observable, oldValue, newValue) -> {
             filteredStaff.setPredicate(s -> {
                 if (newValue == null || newValue.isEmpty() || newValue.equals("All")) {
@@ -316,18 +315,18 @@ public class AdministratorController implements IController {
                 return false;
             });
         });
-        
+
         SortedList<Staff> sortedStaff = new SortedList<>(filteredStaff);
         sortedStaff.comparatorProperty().bind(tblStaff.comparatorProperty());
         tblStaff.setItems(sortedStaff);
-        
+
         colFirstName.setCellValueFactory(new PropertyValueFactory("firstName"));
         colLastName.setCellValueFactory(new PropertyValueFactory("lastName"));
         colUsername.setCellValueFactory(new PropertyValueFactory("username"));
         colDepartment.setCellValueFactory(new PropertyValueFactory("department"));
         colPermission.setCellValueFactory(new PropertyValueFactory("permission"));
     }
-    
+
     private void resetAddUserTab(){
         txfAddFirstName.setText("");
         txfAddLastName.setText("");
@@ -336,31 +335,31 @@ public class AdministratorController implements IController {
         txfAddUsername.setText("");
         cbxAddPermission.setValue("");
     }
-    
+
     private void initPermissionCombobox(){
-       ObservableList<String> permissions = 
+       ObservableList<String> permissions =
                FXCollections.observableArrayList(Permission.ADMIN.toString(),
                Permission.EDITOR.toString(), Permission.VIEWER.toString());
        cbxAddPermission.setItems(permissions);
     }
-    
+
     private void initSearchCombobox(){
-        ObservableList<String> permissions = 
+        ObservableList<String> permissions =
                 FXCollections.observableArrayList("All", Permission.ADMIN.toString(),
                 Permission.EDITOR.toString(), Permission.VIEWER.toString());
         cbxFilterPermissions.setItems(permissions);
         cbxFilterPermissions.setValue("All");
      }
-    
+
     private void resetPassword(){
         SelectionModel<Staff> selection = tblStaff.getSelectionModel();
         Alert alert;
-        if(selection.getSelectedItem()==null){
+        if (selection.getSelectedItem() == null) {
             alert = new Alert(AlertType.INFORMATION);
             alert.setTitle("Reseting password");
             alert.setHeaderText("Information");
             alert.setContentText("Select a user");
-            alert.showAndWait();   
+            alert.showAndWait();
             return;
         }
         String username = selection.getSelectedItem().getUsername();
@@ -368,50 +367,48 @@ public class AdministratorController implements IController {
         int hashedRandomPass = randomPass.hashCode();
         int result = administratorService.resetPassword(
                 username, hashedRandomPass);
-        switch(result){
-        case 200 :
-            alert = new Alert(AlertType.INFORMATION);
-            alert.setTitle("Password reset");
-            alert.setHeaderText("Password reset succesfully");
-            alert.setContentText("new password: "+randomPass);
-            alert.showAndWait();
-            break;
-        
-        case 500 :
-            alert = new Alert(AlertType.ERROR);
-            alert.setTitle("Password reset");
-            alert.setHeaderText("Error during password reset");
-            alert.setContentText("Error occured on the server side");
-            alert.showAndWait();
-            break;
-          }      
+        switch (result) {
+            case 200 :
+                alert = new Alert(AlertType.INFORMATION);
+                alert.setTitle("Password reset");
+                alert.setHeaderText("Password reset succesfully");
+                alert.setContentText("new password: "+randomPass);
+                alert.showAndWait();
+                break;
+            case 500 :
+                alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Password reset");
+                alert.setHeaderText("Error during password reset");
+                alert.setContentText("Error occured on the server side");
+                alert.showAndWait();
+                break;
+          }
     }
-    
+
     private void resetPassword(String username){
         Alert alert;
         String randomPass = administratorService.randomPassword();
         int hashedRandomPass = randomPass.hashCode();
         int result = administratorService.resetPassword(
                 username, hashedRandomPass);
-        switch(result){
-        case 200 :
-            alert = new Alert(AlertType.INFORMATION);
-            alert.setTitle("Password");
-            alert.setHeaderText("Password for user "+username+" set succesfully");
-            alert.setContentText("new password: "+randomPass);
-            alert.showAndWait();
-            break;
-        
-        case 500 :
-            alert = new Alert(AlertType.ERROR);
-            alert.setTitle("Password reset");
-            alert.setHeaderText("Error during password reset");
-            alert.setContentText("Error occured on the server side");
-            alert.showAndWait();
-            break;
-          }  
+        switch (result) {
+            case 200 :
+                alert = new Alert(AlertType.INFORMATION);
+                alert.setTitle("Password");
+                alert.setHeaderText("Password for user "+username+" set succesfully");
+                alert.setContentText("new password: "+randomPass);
+                alert.showAndWait();
+                break;
+            case 500 :
+                alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Password reset");
+                alert.setHeaderText("Error during password reset");
+                alert.setContentText("Error occured on the server side");
+                alert.showAndWait();
+                break;
+          }
     }
-    
+
     @FXML private TextField txfFilterUsers;
     @FXML private ComboBox<String> cbxFilterPermissions;
     @FXML private Button btnResetPassword;
