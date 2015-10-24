b72f31ac1cc5f7add86e1e8b5c03ea35d55e3278
pawc <pswid89@gmail.com>
2015-10-24 18:41:45 +0200

    exporting single case to pdf

--- client/src/main/java/com/kritsit/casetracker/client/domain/services/Export.java
+++ client/src/main/java/com/kritsit/casetracker/client/domain/services/Export.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -14,11 +13,13 @@ import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Font;
+import com.itextpdf.text.ListItem;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.kritsit.casetracker.shared.domain.model.Case;
 
 public class Export implements IExportService{
 
@@ -29,12 +30,12 @@ public class Export implements IExportService{
         Document document = new Document();
 
         try {
-	    logger.info("Creating file output");
+        	logger.info("Creating file output");
             PdfWriter.getInstance(document, new FileOutputStream(file));
             document.open();
             document.add(createTable(headers, cells));
             document.close();
-	    logger.info("export to PDF: success");
+            logger.info("export to PDF: success");
         } catch (DocumentException e) {
             logger.error("Error while exporting to PDF");
             logger.error(e.toString());
@@ -46,7 +47,7 @@ public class Export implements IExportService{
   
     private PdfPTable createTable(List<String> headers, List<String[]> cells){
        
-        PdfPTable table = new PdfPTable(headers.size());
+    PdfPTable table = new PdfPTable(headers.size());
 	Font boldFont = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.BOLD);
 	logger.info("Filling in the table");	
 
@@ -63,4 +64,34 @@ public class Export implements IExportService{
         }           
         return table;           
     }
+
+	public void exportCaseToPDF(Case c, File file) {
+		Document document = new Document();
+
+        try {
+		    logger.info("Creating file output");
+	        PdfWriter.getInstance(document, new FileOutputStream(file));
+	        document.open();
+	        Font titleFont = new Font(Font.FontFamily.TIMES_ROMAN, 18, Font.BOLD);
+	        Font normalFont = new Font(Font.FontFamily.TIMES_ROMAN, 12);
+	        
+	        document.add(new Paragraph("Case no. "+c.getNumber(), titleFont));
+	        document.add(new Paragraph("Investigating officer:  "+c.getInvestigatingOfficer().getLastName()+" "+c.getInvestigatingOfficer().getFirstName(), normalFont));
+	        document.add(new Paragraph("Incident date: "+c.getIncident().getDate(), normalFont));
+	        document.add(new Paragraph(c.getDescription(), normalFont));
+	        document.add(new Paragraph("Evidence: ", normalFont));
+	        com.itextpdf.text.List list = new com.itextpdf.text.List();
+	        	for(int i=0; i < c.getEvidence().size(); i++){
+	        		list.add(new ListItem(c.getEvidence().get(i).getDescription()));
+	        	}
+            document.close();
+            logger.info("export to PDF: success");
+        } catch (DocumentException e) {
+            logger.error("Error while exporting to PDF");
+            logger.error(e.toString());
+        } catch (FileNotFoundException e) {
+            logger.error("Error while exporting to PDF");
+            logger.error(e.toString());
+        }       
+	}
 }
--- client/src/test/java/com/kritsit/casetracker/client/domain/services/ExportTest.java
+++ client/src/test/java/com/kritsit/casetracker/client/domain/services/ExportTest.java
@@ -1,8 +1,10 @@
 package com.kritsit.casetracker.client.domain.services;
 
+import static org.mockito.Mockito.mock;
+
+import java.time.LocalDate;
 import java.util.List;
 import java.util.ArrayList;
-
 import java.io.File;
 import java.nio.file.Files;
 
@@ -11,6 +13,14 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 import java.io.IOException;
+
+import com.kritsit.casetracker.shared.domain.model.Case;
+import com.kritsit.casetracker.shared.domain.model.Defendant;
+import com.kritsit.casetracker.shared.domain.model.Evidence;
+import com.kritsit.casetracker.shared.domain.model.Incident;
+import com.kritsit.casetracker.shared.domain.model.Permission;
+import com.kritsit.casetracker.shared.domain.model.Person;
+import com.kritsit.casetracker.shared.domain.model.Staff;
 
 public class ExportTest extends TestCase {
 
@@ -47,5 +57,39 @@ public class ExportTest extends TestCase {
         } 
         assertFalse(file.exists());
     }  
+    
+    public void testExportCase(){
+    	IExportService exportService = new Export();
+    	
+    	Staff user = mock(Staff.class);
+		String caseNumber = "2015-02-0001";
+		String caseName = "Developers vs Testing";
+		String caseType = "Battle to the death";
+		String details = "Last man standing survives";
+		String animalsInvolved = "1 Developer and 1 AI";
+		LocalDate incidentDate = LocalDate.parse("2014-05-14");
+		String address = "";
+		double longitude = -12.9880;
+		double latitude = 9.82203;
+		String region = "Outer space";
+		boolean isReturnVisit = false;
+		List<Evidence> evidence = new ArrayList<>();
+		Staff investigatingOfficer = new Staff("inspector", "test", "inspector", "department", "position", Permission.EDITOR);
+		Person complainant = new Person(-1, "0212202", "test", "complainant", "Somewhere", "0299222", "test@test.com");
+		Defendant defendant = new Defendant(-1, "0212202", "test", "complainant", "Somewhere", "0299222", "test@test.com", false);
+		Incident incident = new Incident(-1, longitude, latitude, region, incidentDate, Incident.getDefaultFollowUpDate(incidentDate), false);
+		Case c = new Case(caseNumber, caseName, details, animalsInvolved, user, incident, defendant, complainant, null, evidence, isReturnVisit, null, caseType, null);
+
+    	File file = new File("test2.pdf");
+        exportService.exportCaseToPDF(c, file);
+        assertTrue(file.exists());
+        try{
+            file.delete();
+        }
+        catch(Exception e){
+            fail();
+        } 
+        assertFalse(file.exists()); 
+    }
     
 }
